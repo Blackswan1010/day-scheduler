5e91f3321cef6b0da383e047c48c4738494eea7e
Anthony Nguyen <anthonynguye56@gmail.com>
2023-10-05 20:15:59 -0700

    adding developer comments, removing starter code comments, displaying time, implementing code to save and pull from the local storage

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# day-scheduler
+# Day-Scheduler
+
+## Technology Used
+
+| Technology Used         | Resource URL           | 
+| ------------- |:-------------:| 
+| HTML    | [https://developer.mozilla.org/en-US/docs/Web/HTML](https://developer.mozilla.org/en-US/docs/Web/HTML) | 
+| CSS     | [https://developer.mozilla.org/en-US/docs/Web/CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)      |   
+| Git | [https://git-scm.com/](https://git-scm.com/)     | 
+| JavaScript | [https://developer.mozilla.org/en-US/docs/Web/JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)  |
+| JQuery | [https://api.jquery.com](https://api.jquery.com/)  |
+
+## Description
+
+Making a Day-planner with JQuery!
+
+Deployed Site []()
+
+## Jquery Examples
+
+
+
+## Learning Points
+
+
+
+## Author Info
+
+#### Anthony Nguyen
+```md
+* [Github](https://github.com/Blackswan1010)
+```
+
+## Credits
+
+Everyone in bootcamp
+
+## License
+
+N/A
--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,59 @@
-// Declaring a variable that is the root for traversing the DOM
+// Declaring a variable that is the root
 var mainDiv = $('#mainDiv');
-var hourId = [];
 var currentTime = dayjs();
 var currentHour = currentTime.hour();
-var dateDisplay = $('<h2>');
+var dateDisplayEl = $('<h2>');
 var eventText;
-
-$('header').append(dateDisplay.text(currentTime.format('YYYY-MM-DD')));
 
 // Setting the hour variable for day planner to start at the 9th hour
 var hour = 9;
 
-$(function () {
-  // Creating multiple time blocks for a 9am to 5pm day planner
-  for (var i = 0; i < 9; i++) {
-    // Creating the id key for the timeblock, 'hour-x'
-    hourId[i] = 'hour-' + hour.toString();
+// Appending the tag on the header
+$('header').append(dateDisplayEl);
 
-    // Creating new tags within the timeblock
+// Displays the time in the header
+function timeDisplay() {
+  var time = currentTime.format('YYYY-MM-DD [at] hh:mm:ss A')
+  dateDisplayEl.text(time);
+}
+
+// Displays the events pulled fromthe local storage
+function displayEvents() {
+  for (var i = 9; i < 18; i++) {
+    // Create the key based on the hour
+    var key = 'hour-' + i.toString();
+    var eventText = localStorage.getItem(key);
+
+    // Find the corresponding time block and set its textarea value
+    $('#' + key).find('textarea').val(eventText);
+  }
+}
+
+
+$(function () { 
+  // Create multiple time blocks for 9am to 5pm day planner
+  for (var i = 0; i < 9; i++) {
+    // Create new tags within the timeblock
     var newDiv = $('<div>');
     var anotherDiv = $('<div>');
     var newText = $('<textarea>');
     var newButton = $('<button>');
     var newI = $('<i>');
 
-    // Setting the id attribute with the current iteration value in the array 'hourId'
-    newDiv.attr('id', hourId[i]);
-    // Checking the current time to the hour to appropriately add the class 'past', 'present', and 'future'
-    if (currentTime < hour) {
+    // Set and create the id attribute with the current iteration value in the array 'hourId'
+    newDiv.attr('id', 'hour-' + hour.toString());
+    // Check the current time to the hour to appropriately add the class 'past', 'present', and 'future'
+    if (currentHour < hour) {
       newDiv.addClass('row time-block future');
     }
-    if (currentTime === hour) {
+    if (currentHour === hour) {
       newDiv.addClass('row time-block present');
     }
-    if (currentTime > hour) {
+    if (currentHour > hour) {
       newDiv.addClass('row time-block past');
     }
 
-    // Checking the 
+    // Checking the hour to add the appropriate suffix to display on the day planner
     anotherDiv.addClass('col-2 col-md-1 hour text-center py-3');
     if (hour > 12) {
       anotherDiv.text(hour - 12 + 'PM');
@@ -48,7 +64,7 @@ $(function () {
     }
     hour++;
 
-    // Adding classes and setting attributes to the elements textarea, button, and italic
+    // Adding classes and setting attributes to the element tags textarea, button, and italic
     newText.addClass('col-8 col-md-10 description'); //
     newText.attr('rows', '3');
 
@@ -58,46 +74,26 @@ $(function () {
     newI.addClass('as fa-save');
     newI.attr('aria-hidden', 'true');
 
-    // Appending the elements to the mainDiv
+    // Appending the above elements to the mainDiv
     mainDiv.append(newDiv);
     newDiv.append(anotherDiv, newText, newButton);
     newButton.append(newI);
 
-    // Retrieving values from local storage to render on the page
-    eventText = localStorage.getItem(hourId[i]);
-    newText.text(eventText);
+    displayEvents();
   }
 
-
-
+  // When button is clicked the key-value pair is saved to local storage
   $('button').on('click', (function (event) {
-    event.preventDefault(); 
+    event.preventDefault();
+
     var idValue = $(event.target).parent().attr('id');
-    var input = $(event.target).parent().siblings().eq(1).val();
-    console.log(idValue, input);
+    var input = $(event.target).siblings('textarea').val();
+
     localStorage.setItem(idValue, input);
+    
   }));
 
-
 });
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-// TODO: Add code to display the current date in the header of the page.
+
+timeDisplay();
+setInterval(timeDisplay, 1000);
